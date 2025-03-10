a05293ced5d8c4f3d86e1173306ae24a3856d7d7
Julio Sampaio <juliodssampaio@gmail.com>
2025-03-10 23:02:34 +0000

    feat: configure Tailwind CSS

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -1,7 +1,24 @@
+import "./globals.css";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="h-screen w-screen bg-support text-primary">
+        <header className="border-b-2 border-primary flex pr-5 pl-5">
+          <ul className="flex gap-4 items-end">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+          </ul>
+        </header>
+        <main className="p-5">
+          {children}
+          <p className="border-t-2 border-b-2 border-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+        </main>
+      </body>
     </html>
   );
 }
