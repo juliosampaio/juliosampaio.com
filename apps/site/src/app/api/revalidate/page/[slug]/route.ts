fba3f44b7fc29b5255fd12c9caad4ac51fc837cd
Julio Sampaio <juliodssampaio@gmail.com>
2025-03-19 22:16:51 +0000

    fix: lint issues

--- apps/site/src/app/api/revalidate/page/[slug]/route.ts
+++ apps/site/src/app/api/revalidate/page/[slug]/route.ts
@@ -3,7 +3,10 @@ import { API_VERSION, createClient } from '@juliosampaio/cms-client'
 export async function POST(request: Request) {
   const client = createClient()
 
-  console.log({ API_VERSION })
+  console.log({
+    API_VERSION,
+    request,
+  })
 
   return new Response(await client.getPage('pageName'))
 }
